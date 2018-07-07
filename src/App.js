7402e517fb9e27af3ce3ec7981d4696511ab474f
Miro86 <mzoranovic86@gmail.com>
2018-07-07 19:55:03 +0200

    added particles.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
 import './App.css';
 import Header from '../src/Components/Header';
+import Particles from 'react-particles-js';
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 150,
+      density: {
+        enable: true
+      }
+    }
+  }
+};
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Particles id="particles" params={particlesOptions} />
         <Header />
       </div>
     );
