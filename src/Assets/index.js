7402badf5f6c198ac47432a2ef3d670e963ebdf9
Miro86 <mzoranovic86@gmail.com>
2018-07-13 11:57:04 +0200

    added new assets and implemented them

--- src/Assets/index.js
+++ src/Assets/index.js
@@ -10,102 +10,121 @@ import youtube from './Icons/youtube.png';
 import logo from './logo.png';
 
 import babyPhoneCover from './Games/BabyPhone/BabyPhoneCover.png';
+import babyPhoneCoverSmall from './Games/BabyPhone/BabyPhoneCoverSmall.png';
 import babyPhone1 from './Games/BabyPhone/BabyPhone1.png';
 import babyPhone2 from './Games/BabyPhone/BabyPhone2.png';
 import babyPhone3 from './Games/BabyPhone/BabyPhone3.png';
 import babyPhone4 from './Games/BabyPhone/BabyPhone4.png';
 
 import balkanRoadsCover from './Games/BalkanRoads/BalkanRoadsCover.png';
+import balkanRoadsCoverSmall from './Games/BalkanRoads/BalkanRoadsCoverSmall.png';
 import balkanRoads1 from './Games/BalkanRoads/BalkanRoads1.png';
 import balkanRoads2 from './Games/BalkanRoads/BalkanRoads2.png';
 import balkanRoads3 from './Games/BalkanRoads/BalkanRoads3.png';
 
 import bankEscapeCover from './Games/BankEscape/BankEscapeCover.png';
+import bankEscapeCoverSmall from './Games/BankEscape/BankEscapeCoverSmall.png';
 import bankEscape1 from './Games/BankEscape/BankEscape1.png';
 import bankEscape2 from './Games/BankEscape/BankEscape2.png';
 import bankEscape3 from './Games/BankEscape/BankEscape3.png';
 import bankEscape4 from './Games/BankEscape/BankEscape4.png';
 
 import bigBenCover from './Games/BigBen/BigBenCover.png';
+import bigBenCoverSmall from './Games/BigBen/BigBenCoverSmall.png';
 import bigBen1 from './Games/BigBen/BigBen1.png';
 import bigBen2 from './Games/BigBen/BigBen2.png';
 import bigBen3 from './Games/BigBen/BigBen3.png';
 
 import cowboyDuelCover from './Games/CowboyDuel/CowboyDuelCover.png';
+import cowboyDuelCoverSmall from './Games/CowboyDuel/CowboyDuelCoverSmall.png';
 import cowboyDuel1 from './Games/CowboyDuel/CowboyDuel1.png';
 import cowboyDuel2 from './Games/CowboyDuel/CowboyDuel2.png';
 import cowboyDuel3 from './Games/CowboyDuel/CowboyDuel3.png';
 
 import doodleFidgetSpinnerCover from './Games/DoodleFidgetSpinner/DoodleFidgetSpinnerCover.png';
+import doodleFidgetSpinnerCoverSmall from './Games/DoodleFidgetSpinner/DoodleFidgetSpinnerCoverSmall.png';
 import doodleFidgetSpinner1 from './Games/DoodleFidgetSpinner/DoodleFidgetSpinner1.png';
 import doodleFidgetSpinner2 from './Games/DoodleFidgetSpinner/DoodleFidgetSpinner2.png';
 import doodleFidgetSpinner3 from './Games/DoodleFidgetSpinner/DoodleFidgetSpinner3.png';
 import doodleFidgetSpinner4 from './Games/DoodleFidgetSpinner/DoodleFidgetSpinner4.png';
 
 import eMojiMachineCover from './Games/EMojiMachine/EMojiMachineCover.png';
+import eMojiMachineCoverSmall from './Games/EMojiMachine/EMojiMachineCoverSmall.png';
 import eMojiMachine1 from './Games/EMojiMachine/EMojiMachine1.png';
 import eMojiMachine2 from './Games/EMojiMachine/EMojiMachine2.png';
 import eMojiMachine3 from './Games/EMojiMachine/EMojiMachine3.png';
 import eMojiMachine4 from './Games/EMojiMachine/EMojiMachine4.png';
 
 import fiveSecondsStressTestCover from './Games/FiveSecondsStressTest/FiveSecondsStressTestCover.png';
+import fiveSecondsStressTestCoverSmall from './Games/FiveSecondsStressTest/FiveSecondsStressTestCoverSmall.png';
 import fiveSecondsStressTest1 from './Games/FiveSecondsStressTest/FiveSecondsStressTest1.png';
 import fiveSecondsStressTest2 from './Games/FiveSecondsStressTest/FiveSecondsStressTest2.png';
 
 import gastarbajterCover from './Games/Gastarbajter/GastarbajterCover.png';
+import gastarbajterCoverSmall from './Games/Gastarbajter/GastarbajterCoverSmall.png';
 import gastarbajter1 from './Games/Gastarbajter/Gastarbajter1.png';
 import gastarbajter2 from './Games/Gastarbajter/Gastarbajter2.png';
 import gastarbajter3 from './Games/Gastarbajter/Gastarbajter3.png';
 
 import godVsStickmanCover from './Games/GodVsStickman/GodVsStickmanCover.png';
+import godVsStickmanCoverSmall from './Games/GodVsStickman/GodVsStickmanCoverSmall.png';
 import godVsStickman1 from './Games/GodVsStickman/GodVsStickman1.png';
 import godVsStickman2 from './Games/GodVsStickman/GodVsStickman2.png';
 import godVsStickman3 from './Games/GodVsStickman/GodVsStickman3.png';
 
 import kissTheBossCover from './Games/KissTheBoss/KissTheBossCover.png';
+import kissTheBossCoverSmall from './Games/KissTheBoss/KissTheBossCoverSmall.png';
 import kissTheBoss1 from './Games/KissTheBoss/KissTheBoss1.png';
 import kissTheBoss2 from './Games/KissTheBoss/KissTheBoss2.png';
 import kissTheBoss3 from './Games/KissTheBoss/KissTheBoss3.png';
 
 import mindReaderCover from './Games/MindReader/MindReaderCover.png';
+import mindReaderCoverSmall from './Games/MindReader/MindReaderCoverSmall.png';
 import mindReader1 from './Games/MindReader/MindReader1.png';
 import mindReader2 from './Games/MindReader/MindReader2.png';
 import mindReader3 from './Games/MindReader/MindReader3.png';
 
 import naymarChallengeCover from './Games/NaymarChallenge/NaymarChallengeCover.png';
+import naymarChallengeCoverSmall from './Games/NaymarChallenge/NaymarChallengeCoverSmall.png';
 import naymarChallenge1 from './Games/NaymarChallenge/NaymarChallenge1.png';
 import naymarChallenge2 from './Games/NaymarChallenge/NaymarChallenge2.png';
 import naymarChallenge3 from './Games/NaymarChallenge/NaymarChallenge3.png';
 import naymarChallenge4 from './Games/NaymarChallenge/NaymarChallenge4.png';
 
 import podMac555Cover from './Games/PodMac555/PodMac555Cover.png';
+import podMac555CoverSmall from './Games/PodMac555/PodMac555CoverSmall.png';
 import podMac5551 from './Games/PodMac555/PodMac5551.png';
 import podMac5552 from './Games/PodMac555/PodMac5552.png';
 import podMac5553 from './Games/PodMac555/PodMac5553.png';
 
 import podMacBatoCover from './Games/PodMacBato/PodMacBatoCover.png';
+import podMacBatoCoverSmall from './Games/PodMacBato/PodMacBatoCoverSmall.png';
 import podMacBato1 from './Games/PodMacBato/PodMacBato1.png';
 import podMacBato2 from './Games/PodMacBato/PodMacBato2.png';
 import podMacBato3 from './Games/PodMacBato/PodMacBato3.png';
 
 import santaTheLostPresentsCover from './Games/SantaTheLostPresents/SantaTheLostPresentsCover.png';
+import santaTheLostPresentsCoverSmall from './Games/SantaTheLostPresents/SantaTheLostPresentsCoverSmall.png';
 import santaTheLostPresents1 from './Games/SantaTheLostPresents/SantaTheLostPresents1.png';
 import santaTheLostPresents2 from './Games/SantaTheLostPresents/SantaTheLostPresents2.png';
 import santaTheLostPresents3 from './Games/SantaTheLostPresents/SantaTheLostPresents3.png';
 
 import theBirdsCover from './Games/TheBirds/TheBirdsCover.png';
+import theBirdsCoverSmall from './Games/TheBirds/TheBirdsCoverSmall.png';
 import theBirds1 from './Games/TheBirds/TheBirds1.png';
 import theBirds2 from './Games/TheBirds/TheBirds2.png';
 import theBirds3 from './Games/TheBirds/TheBirds3.png';
 import theBirds4 from './Games/TheBirds/TheBirds4.png';
 
 import zeroDeathsCover from './Games/ZeroDeaths/ZeroDeathsCover.png';
+import zeroDeathsCoverSmall from './Games/ZeroDeaths/ZeroDeathsCoverSmall.png';
 import zeroDeaths1 from './Games/ZeroDeaths/ZeroDeaths1.png';
 import zeroDeaths2 from './Games/ZeroDeaths/ZeroDeaths2.png';
 import zeroDeaths3 from './Games/ZeroDeaths/ZeroDeaths3.png';
 import zeroDeaths4 from './Games/ZeroDeaths/ZeroDeaths4.png';
 
 import zigZagSkyCover from './Games/ZigZagSky/ZigZagSkyCover.png';
+import zigZagSkyCoverSmall from './Games/ZigZagSky/ZigZagSkyCoverSmall.png';
 import zigZagSky1 from './Games/ZigZagSky/ZigZagSky1.png';
 import zigZagSky2 from './Games/ZigZagSky/ZigZagSky2.png';
 import zigZagSky3 from './Games/ZigZagSky/ZigZagSky3.png';
@@ -136,20 +155,40 @@ const GameCovers = [
 const SocialMediaIcons = [facebook, youtube, twitter, instagram];
 
 const LatestGamesCovers = [
-  zeroDeathsCover,
-  bigBenCover,
-  gastarbajterCover,
-  naymarChallengeCover,
-  cowboyDuelCover,
-  bankEscapeCover,
-  zigZagSkyCover,
-  theBirdsCover,
-  eMojiMachineCover
+  zeroDeathsCoverSmall,
+  bigBenCoverSmall,
+  gastarbajterCoverSmall,
+  naymarChallengeCoverSmall,
+  cowboyDuelCoverSmall,
+  bankEscapeCoverSmall,
+  zigZagSkyCoverSmall,
+  theBirdsCoverSmall,
+  eMojiMachineCoverSmall
 ];
 
 const LogoImage = logo;
 
-const AllGames = GameCovers;
+const AllGames = [
+  zeroDeathsCoverSmall,
+  gastarbajterCoverSmall,
+  naymarChallengeCoverSmall,
+  bigBenCoverSmall,
+  theBirdsCoverSmall,
+  zigZagSkyCoverSmall,
+  santaTheLostPresentsCoverSmall,
+  podMac555CoverSmall,
+  podMacBatoCoverSmall,
+  mindReaderCoverSmall,
+  kissTheBossCoverSmall,
+  godVsStickmanCoverSmall,
+  fiveSecondsStressTestCoverSmall,
+  eMojiMachineCoverSmall,
+  doodleFidgetSpinnerCoverSmall,
+  cowboyDuelCoverSmall,
+  bankEscapeCoverSmall,
+  balkanRoadsCoverSmall,
+  babyPhoneCoverSmall
+];
 
 export { LogoImage };
 export { GameCovers };
