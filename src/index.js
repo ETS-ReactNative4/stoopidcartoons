7527713c9144a24dfface7e22e3d68b0d525b924
MiroslavRadulovic <miroslav.radulovic92@gmail.com>
2018-07-16 21:15:53 +0200

    soe more evangelism

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 import { BrowserRouter as Router } from "react-router-dom";
+import "tachyons";
 
 ReactDOM.render(
   <Router>
