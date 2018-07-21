1c50f16e5d0c23ad81ab6ef593d396c190d24238
Miro86 <mzoranovic86@gmail.com>
2018-07-21 16:05:04 +0200

    .

--- src/Components/Logo.js
+++ src/Components/Logo.js
@@ -1,8 +1,8 @@
-import React from "react";
-import "../Css/Logo.css";
-import { LogoImage } from "../Assets/index";
-import { Link } from "react-router-dom";
-import Tilt from "react-tilt";
+import React from 'react';
+import '../Css/Logo.css';
+import { LogoImage } from '../Assets/index';
+import { NavLink } from 'react-router-dom';
+import Tilt from 'react-tilt';
 
 const Logo = () => {
   return (
@@ -12,11 +12,11 @@ const Logo = () => {
         options={{ max: 55 }}
         style={{ width: 150 }}
       >
-        <Link to="/" style={{ padding: "0" }} >
+        <NavLink to="/" style={{ padding: '0' }}>
           <div className="Tilt-inner pa3">
             <img id="logo" src={LogoImage} alt="" />
           </div>
-        </Link>
+        </NavLink>
       </Tilt>
     </div>
   );
